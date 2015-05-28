18cbff169c0bdfe488929fc39eba0573e4c1e44a
Андрей <anechytailov@gmail.com>
2015-05-28 22:38:11 +0300

    Fixed broken dart server

--- slides/bin/server.dart
+++ slides/bin/server.dart
@@ -1,19 +1,28 @@
-import "dart:io";
+import 'dart:io';
+import 'dart:async' show runZoned;
+import 'package:http_server/http_server.dart' show VirtualDirectory;
 
-import 'package:redstone/server.dart' as app;
-import 'package:shelf_static/shelf_static.dart';
+void main() {
+  var port = int.parse(_getConfig("PORT", "8080"));
+  var web = _getConfig("WEB_FOLDER", '../build/web');
 
-main() {
+  // Assumes the server lives in bin/ and that `pub build` ran.
+  var buildUri = Platform.script.resolve(web);
 
-  var port = _getConfig("PORT", "8080");
-  var web = _getConfig("WEB_FOLDER", "web");
+  var staticFiles = new VirtualDirectory(buildUri.toFilePath());
+  staticFiles
+    ..allowDirectoryListing = true
+    ..directoryHandler = (dir, request) {
+    var indexUri = new Uri.file(dir.path).resolve('index.html');
+    var uri = indexUri.toFilePath();
+    staticFiles.serveFile(new File(uri), request);
+  };
 
-  app.setShelfHandler(
-      createStaticHandler(web,  defaultDocument: "index.html", serveFilesOutsidePath: true));
-
-  app.setupConsoleLog();
-
-  app.start(port: int.parse(port));
+  runZoned(() {
+    HttpServer.bind(InternetAddress.ANY_IP_V4, port).then((server) {
+      server.listen(staticFiles.serveRequest);
+    });
+  }, onError: (e, stackTrace) => print('Error: $e $stackTrace'));
 }
 
 _getConfig(String name, [defaultValue]) {
